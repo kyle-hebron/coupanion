2d09b88aca6a961431f474dea2e827dc395a3db1
abelbark <106646800+abelbark@users.noreply.github.com>
2022-10-06 23:00:48 -0700

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'PaleGoldenRod',
+    backgroundColor: 'palegoldenrod',
     alignItems: 'center',
     justifyContent: 'center',
   },
