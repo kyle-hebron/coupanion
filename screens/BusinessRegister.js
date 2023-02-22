2c35fc322712a5c4bb40b2285bf70b431e62f0e4
abelbark <106646800+abelbark@users.noreply.github.com>
2023-02-21 20:01:50 -0800

    Update BusinessRegister.js

--- screens/BusinessRegister.js
+++ screens/BusinessRegister.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import Icon from "react-native-vector-icons/FontAwesome5";
 
@@ -6,8 +6,23 @@ import GradientIconButton from '../components/GradientIconButton';
 import GradientTextButton from '../components/GradientTextButton';
 import PlainInput from '../components/PlainInput';
 
-export default function RegisterScreen({navigation}){
+import { db } from '../firebase'
+import { onAuthStateChanged } from "firebase/auth";
+import { getAuth, updateProfile} from "firebase/auth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+
+
+export default function BusinessRegister({navigation}){
     //const {isBusiness} = route.params;
+    const [address1, setAddress1] = useState('')
+    const [address2, setAddress2] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [zip, setZip] = useState('')
+    const auth = getAuth();
+    const user = auth.currentUser;
+    
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.top}>
@@ -18,21 +33,46 @@ export default function RegisterScreen({navigation}){
                 </TouchableOpacity>
             </View>
             <View styles={styles.middle}>
-                <Text style={styles.title}>Business Address</Text>
-                <PlainInput icon="house-user" text="Address Line 1"  secure={false} />
-                <PlainInput icon="house-user" text="Address Line 2"  secure={false} />
-                <PlainInput icon="house-user" text="City"  secure={false} />
-                <PlainInput icon="house-user" text="State"  secure={false} />
-                <PlainInput icon="house-user" text="ZIP"  secure={false} />
+              <Text style={styles.title}>Business Address</Text>
+                <PlainInput icon="house-user" text="Address Line 1"  secure={false} handleChange={setAddress1}/>
+                <PlainInput icon="house-user" text="Address Line 2"  secure={false} handleChange={setAddress2}/>
+                <PlainInput icon="house-user" text="City"  secure={false} handleChange={setCity}/>
+                <PlainInput icon="house-user" text="State"  secure={false} handleChange={setState}/>
+                <PlainInput icon="house-user" text="ZIP"  secure={false} handleChange={setZip}/>
             </View>
             <View styles={styles.bottom}>
-                <TouchableOpacity onPress={() => {navigation.navigate('CouponMaker')}}>
+                <TouchableOpacity onPress={() => {signAddress(address1, address2, city, state, zip)}}>
                     <GradientTextButton text={"Sign Up"} styles={styles} />
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
+    
+    function signAddress(address1, address2, city, state, zip){
+        if(user){
+                setDoc(doc(db, "Business people", user.uid),{
+                    AddressInfo: {
+                        address1: address1,
+                        address2: address2,
+                        city: city,
+                        state: state,
+                        zip: zip,
+                    } 
+
+                }, {merge: true});
+
+                navigation.navigate('CouponMaker');
+
+
+            } else {
+                console.log("No Business Created")
+                setError("No Business Created")
+            }
+         
+    }
+    
 }
+
 
 const styles = StyleSheet.create({
     container: {
