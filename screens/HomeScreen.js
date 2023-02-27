e12c35e53e67624f6d6eb57bb304a3008c27c626
JsakoCS <104119408+JsakoCS@users.noreply.github.com>
2023-02-27 01:53:45 -0800

    Updates to HomeScreen.js .

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,159 +1,153 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, Image, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, FlatList, SafeAreaView } from 'react-native';
+import { db } from '../firebase';
 
-export default function HomeScreen({navigation}) {
-  const business = [
-    {
-        id: 1,
-        name: "Porto's Bakery",
-        tag: 'Bakery',
-        image: require('../assets/logos/portos.png')
-    },
-    {
-        id: 2,
-        name: "Anne's Pastries",
-        tag: 'Bakery',
-        image: require('../assets/logos/restuarant.png')
-    },
-    {
-        id: 3,
-        name: "The Italian",
-        tag: 'Restaurant',
-        image: require('../assets/logos/company.png')
-    }
-  ]
-  const nearby = [
-    {
-      id: 4,
-      name: "Pizza Palace",
-      tag: 'Restaurant',
-      image: require('../assets/logos/pizza.png')
-  },
-  {
-      id: 5,
-      name: "Olive Garden",
-      tag: 'Restaurant',
-      image: require('../assets/logos/olive.png')
-  },
-  {
-      id: 6,
-      name: "Lily's Flowers",
-      tag: 'Shop',
-      image: require('../assets/logos/flower.jpg')
-  },
-  ]
-    return(
-        <SafeAreaView style={styles.appContainer}>
-          <ScrollView>
-        <View style={styles.inputContainer}>
-          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Discover</Text>
-        </View>
-        <View>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Trending</Text>
-        </View>
+export default function HomeScreen({ navigation }) {
+  const [trendingBusinesses, setTrendingBusinesses] = useState([]);
+  const [topRatedBusinesses, setTopRatedBusinesses] = useState([]);
+
+  useEffect(() => {
+    // Get trending businesses from Firestore .
+    const trendingBusinessesRef = db.collection('Business people').where('trending', '==', true);
+    const unsubscribeTrending = trendingBusinessesRef.onSnapshot((snapshot) => {
+      const businesses = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        businesses.push({
+          id: doc.id,
+          name: data.username,
+          tag: data.tag,
+          image: { uri: data.image }
+        });
+      });
+      setTrendingBusinesses(businesses);
+    });
+
+    // Get top rated businesses from Firestore .
+    const topRatedBusinessesRef = db.collection('Business people').where('top rated', '==', true);
+    const unsubscribeTopRated = topRatedBusinessesRef.onSnapshot((snapshot) => {
+      const businesses = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        businesses.push({
+          id: doc.id,
+          name: data.username,
+          tag: data.tag,
+          image: { uri: data.image }
+        });
+      });
+      setTopRatedBusinesses(businesses);
+    });
+
+    return () => {
+      unsubscribeTrending();
+      unsubscribeTopRated();
+    };
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Image source={item.image} style={styles.logo} />
+      <View style={styles.nameContainer}>
+        <Text style={styles.text}>{item.name}</Text>
+        <Text style={styles.tag}>{item.tag}</Text>
+      </View>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>E x p l o r e</Text>
+      </View>
+      <View style={styles.trendingContainer}>
+        <Text style={styles.trendingText}>Trending</Text>
         <FlatList
-            data = {business}
-            contentContainerStyle = {styles.container}
-            renderItem = {({item}) => {
-                    return(
-                    <View style={styles.item}>
-                    <Image source={item.image} style={styles.logo} />
-                <View style={styles.nameContainer}>
-                    <Text style={styles.text}>{item.name}</Text>
-                    <Text style={styles.tag}>{item.tag}</Text>
-                    </View>
-                </View>
-                )
-            }}/>
-        <View style={{ paddingTop: 20 }}>
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>Top Rated</Text>
-        </View>
+          data={trendingBusinesses}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+      <View style={styles.topRatedContainer}>
+        <Text style={styles.topRatedText}>Top Rated</Text>
         <FlatList
-            data = {nearby}
-            contentContainerStyle = {styles.container}
-            renderItem = {({item}) => {
-                    return(
-                    <View style={styles.item}>
-                    <Image source={item.image} style={styles.logo} />
-                <View style={styles.nameContainer}>
-                    <Text style={styles.text}>{item.name}</Text>
-                    <Text style={styles.tag}>{item.tag}</Text>
-                    </View>
-                </View>
-                )
-            }}/>
-        <View>
-          <Text style={{ fontSize: 20, fontWeight: "bold", paddingTop: 30 }}>Recently Visited</Text>
-        </View>
-        <View style={{ paddingTop: 20 }}>
-          <Text style={styles.recentContainer}>You haven't visited any businesses recently</Text>
-        </View>
-        </ScrollView>
-      </SafeAreaView>
-    )
+          data={topRatedBusinesses}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-      paddingTop: 20,
-      paddingLeft: 30,
-      paddingRight: 20
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      paddingBottom: 30
-    },
-    TextInput: {
-      borderWidth: 1,
-      borderColor: '#DDDFE0',
-      width: '60%',
-      padding: 5
-    },
-    boxContainer: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      borderWidth: 30,
-      borderColor: '#DDDFE0',
-      backgroundColor: '#DDDFE0',
-      width: '100%'
-    },
-    recentContainer: {
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      borderRadius: 30,
-      borderWidth: 70,
-      borderColor: '#DDDFE0',
-      backgroundColor: '#DDDFE0',
-      color: '#999999',
-      width: '100%'
-    },
     container: {
-      flexDirection: 'column',
-      paddingTop: 5,
-  },
-  nameContainer: {
-      flexDirection: 'column',
-      paddingLeft: 10,
-  },
-  item: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 10,
-  },
-  logo: {
-      height: 100,
-      width: 100, 
-      borderRadius: 100,
-      backgroundColor: '#ffffff',
-  },
-  text: {
-      fontSize: 20,
-      fontWeight: "bold",
-  },
-  tag: {
-      fontSize: 14,
-      color: '#969696',
-      fontWeight: "bold"
-  },
-  })
+      flex: 1,
+      backgroundColor: '#fff',
+      paddingHorizontal: 10,
+      paddingTop: 30,
+    },
+    headerContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingBottom: 10,
+    },
+    headerText: {
+      fontSize: 33,
+      fontWeight: 'bold',
+    },
+    item: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 8,
+      marginHorizontal: 16,
+      paddingVertical: 8,
+      paddingHorizontal: 33,
+      borderRadius: 8,
+      backgroundColor: '#f5f5f5',
+    },
+    logo: {
+      width: 64,
+      height: 64,
+      borderRadius: 32,
+      marginRight: 33,
+    },
+    textContainer: {
+      flex: 1,
+    },
+    text: {
+      fontSize: 19,
+      fontWeight: 'bold',
+      marginBottom: 4,
+    },
+    tag: {
+      fontSize: 13,
+      color: '#666',
+      fontWeight: 'bold',
+    },
+    trendingContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      paddingTop: 16,
+      paddingBottom: 16,
+    },
+    trendingText: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      paddingHorizontal: 16,
+      marginBottom: 16,
+    },
+    topRatedContainer: {
+      flex: 1,
+      backgroundColor: '#fff',
+      paddingTop: 16,
+      paddingBottom: 16,
+    },
+    topRatedText: {
+      fontSize: 25,
+      fontWeight: 'bold',
+      paddingHorizontal: 16,
+      marginBottom: 16,
+    },
+  });
