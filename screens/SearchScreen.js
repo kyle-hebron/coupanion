4a68dd22ba127818fbaa6bea38599cf4d40db07f
smootToast <thewaffleminister@gmail.com>
2023-04-26 17:02:52 -0700

    Search works

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,126 +1,141 @@
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState, useEffect } from "react";
 import {
-	StyleSheet,
-	Text,
-	TextInput,
-	KeyboardAvoidingView,
-	TouchableOpacity,
-	SafeAreaView,
-	View,
-	TouchableWithoutFeedback,
-	Keyboard,
-	FlatList,
-	
+  StyleSheet,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  SafeAreaView,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+  Image,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import GradientIconButton from "../components/GradientIconButton";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 import IconInput from "../components/IconInput";
 
 const SearchScreen = ({ navigation }) => {
-	const [size, setSize] = useState(0);
-	const [input, setInput] = useState("");
-    const business = [];
-	var listBusiness = []; 
-	const [searchList, setList] = useState([ ]);
-	const [bus, setBus] = useState([]);
-	const [hasSearched, setSearch] = useState(false);
-	var temp = 0;
-	useEffect(() => {
-	async function search() {
+  const [size, setSize] = useState(0);
+  const [input, setInput] = useState("");
+  const [business, setBusiness] = useState([]);
+  const [searchList, setSearchList] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
         const q = query(collection(db, "Business people"));
-        
-        
-		
         const querySnapshot = await getDocs(q);
-        
-        
-        let i = 0;
-        querySnapshot.forEach((doc) => {	//Need if statement to only get within a certain radius
-            business[i] = doc.data();
-            i++;
-			setSize(i);
-			setList(business);
-        });
-        
-    
-        
-		
-		console.log(searchList[16].business);
-		
-    } search();
-}, []);
-	function searchData(){
-		setSearch(false);
-		var tempBus = [];
-		for(let i = 0; i < size; i++){
-			if(searchList[i].business == input && searchList[i].uid != undefined){
-				tempBus.push({ id: temp++,
-					"name": searchList[i].business,
-					"userID": searchList[i].uid
-					
-				})
-			}
-		}
-		listBusiness = tempBus;
-		temp = 0;
-		console.log(listBusiness);
-		setSearch(true);
-	};
-	
-	const renderItem = ({ item, index }) => (
-		
-		<TouchableOpacity
-			onPress={() => navigation.navigate("Business", { id: item.id })}
-		>
-			<View style={styles.item}>
-				<Image source={item.image} style={styles.logo} />
-				<View style={styles.nameContainer}>
-					<Text style={styles.text}>{item.name}</Text>
-					
-				</View>
-			</View>
-		</TouchableOpacity>
+        const businesses = querySnapshot.docs.map((doc) => doc.data());
+        setBusiness(businesses);
+        setSearchList(businesses);
+        setSize(businesses.length);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const searchData = () => {
+	setHasSearched(false);
+	if (input.trim() === "") {
+	  setSearchList([]);
+	  setHasSearched(true);
+	  return;
+	}
+	const searchTerm = input.toLowerCase();
+	const tempBus = business.filter(
+	  (item) =>
+		item.business &&
+		item.business.toLowerCase().includes(searchTerm) &&
+		item.uid !== undefined
 	);
-	
-	return (
-		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-			<SafeAreaView style={styles.container}>
-				<KeyboardAvoidingView>
-					<Text style={styles.title}>Search</Text>
-					<View style={styles.searchSection}>
-						
-						<IconInput text="Code"
-							handleChange={setInput}
-							/>
-						<TouchableOpacity onPress={() => searchData()}>
-							<Icon style={styles.icon} name="search" size={30} color="#FFF" />
-						</TouchableOpacity>
-						<Text>Hello</Text>
-						           
-					</View>
-					<View>
-						{hasSearched ? <Text>{listBusiness[0].name}</Text>: <></>}
-					</View>
-				</KeyboardAvoidingView>
-			</SafeAreaView>
-		</TouchableWithoutFeedback>
-	);
+	setSearchList(tempBus);
+	setHasSearched(true);
+  };
+
+
+  const renderItem = ({ item }) => (
+	<TouchableOpacity
+	  onPress={() => navigateToBusiness(item)}
+	  style={styles.button}
+	>
+	  <View style={styles.buttonContainer}>
+		{/* Add image component here if available */}
+		<Text style={styles.buttonText}>{item.business}</Text>
+	  </View>
+	</TouchableOpacity>
+  );
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <KeyboardAvoidingView>
+          <Text style={styles.title}>Search</Text>
+          <View style={styles.searchSection}>
+            <IconInput
+              text="Search"
+              handleChange={setInput}
+            />
+            <TouchableOpacity onPress={searchData}>
+              <Icon style={styles.icon} name="search" size={30} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+          <View>
+		  {hasSearched && (
+    <FlatList
+      data={searchList}
+      renderItem={renderItem}
+      keyExtractor={(item, index) => index.toString()} // Generate unique key using the index
+    />
+  )}
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
 };
 
 export default SearchScreen;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: "#102C54",
-		flex: 1,
+	// ...
+  
+	button: {
+	  backgroundColor: "#FFF",
+	  borderRadius: 30,
+	  paddingVertical: 10,
+	  paddingHorizontal: 20,
+	  marginBottom: 10,
+	  marginLeft: 15,
+	  marginRight: 15,
+	  alignSelf: "stretch",
 	},
-	input: {
-		backgroundColor: "white",
-		paddingHorizontal: 25,
+	buttonContainer: {
+	  flexDirection: "row",
+	  justifyContent: "center",
+	  alignItems: "center",
+	},
+	buttonText: {
+	  color: "#102C54",
+	  fontSize: 18,
+	},
+  container: {
+    backgroundColor: "#102C54",
+    flex: 1,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal
+: 25,
 		paddingVertical: 0,
 		borderRadius: 100,
 		width: "90%",
